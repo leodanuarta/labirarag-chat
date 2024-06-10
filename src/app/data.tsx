ae436f9a51ba2d0589cb75f7ee31741b2812e196
leodanuarta <leo.535210024@stu.untar.ac.id>
2024-06-10 23:15:59 +0700

    update tampilan chat pada RAGLabira UI

--- src/app/data.tsx
+++ src/app/data.tsx
@@ -3,62 +3,62 @@ export const userData = [
         id: 1,
         avatar: '/User1.png',
         messages: [
-            {
-                id: 1,
-                avatar: '/User1.png',
-                name: 'Jane Doe',
-                message: 'Hey, Jakob',
-            },
-            {
-                id: 2,
-                avatar: '/LoggedInUser.jpg',
-                name: 'Jakob Hoeg',
-                message: 'Hey!',
-            },
-            {
-                id : 3,
-                avatar: '/User1.png',
-                name: 'Jane Doe',
-                message: 'How are you?',
-            },
-            {
-                id: 4,
-                avatar: '/LoggedInUser.jpg',
-                name: 'Jakob Hoeg',
-                message: 'I am good, you?',
-            },
-            {
-                id: 5,
-                avatar: '/User1.png',
-                name: 'Jane Doe',
-                message: 'I am good too!',
-            },
-            {
-                id: 6,
-                avatar: '/LoggedInUser.jpg',
-                name: 'Jakob Hoeg',
-                message: 'That is good to hear!'
-            },
-            {
-                id: 7,
-                avatar: '/User1.png',
-                name: 'Jane Doe',
-                message: 'How has your day been so far?',
-            },
-            {
-                id: 8,
-                avatar: '/LoggedInUser.jpg',
-                name: 'Jakob Hoeg',
-                message: 'It has been good. I went for a run this morning and then had a nice breakfast. How about you?',
-            },
-            {
-                id: 9,
-                avatar: '/User1.png',
-                name: 'Jane Doe',
-                message: 'I had a relaxing day. Just catching up on some reading.',
-            }
+            // {
+            //     id: 1,
+            //     avatar: '/User1.png',
+            //     name: 'Jane Doe',
+            //     message: 'Hey, Jakob',
+            // },
+            // {
+            //     id: 2,
+            //     avatar: '/LoggedInUser.jpg',
+            //     name: 'Jakob Hoeg',
+            //     message: 'Hey!',
+            // },
+            // {
+            //     id : 3,
+            //     avatar: '/User1.png',
+            //     name: 'Jane Doe',
+            //     message: 'How are you?',
+            // },
+            // {
+            //     id: 4,
+            //     avatar: '/LoggedInUser.jpg',
+            //     name: 'Jakob Hoeg',
+            //     message: 'I am good, you?',
+            // },
+            // {
+            //     id: 5,
+            //     avatar: '/User1.png',
+            //     name: 'Jane Doe',
+            //     message: 'I am good too!',
+            // },
+            // {
+            //     id: 6,
+            //     avatar: '/LoggedInUser.jpg',
+            //     name: 'Jakob Hoeg',
+            //     message: 'That is good to hear!'
+            // },
+            // {
+            //     id: 7,
+            //     avatar: '/User1.png',
+            //     name: 'Jane Doe',
+            //     message: 'How has your day been so far?',
+            // },
+            // {
+            //     id: 8,
+            //     avatar: '/LoggedInUser.jpg',
+            //     name: 'Jakob Hoeg',
+            //     message: 'It has been good. I went for a run this morning and then had a nice breakfast. How about you?',
+            // },
+            // {
+            //     id: 9,
+            //     avatar: '/User1.png',
+            //     name: 'Jane Doe',
+            //     message: 'I had a relaxing day. Just catching up on some reading.',
+            // }
         ],
-        name: 'Jane Doe',
+        name: 'LABIRA RAG BOT',
     },
     
 ];
